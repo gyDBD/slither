3c5c08fe1d2d45fc5a350c2efbca85a10200f1d9
gyDBD <gydbd06@hotmail.com>
2019-01-16 16:13:03 -0500

    add log

--- slither/__main__.py
+++ slither/__main__.py
@@ -266,6 +266,7 @@ def main_impl(all_detector_classes, all_printer_classes):
                 (results, number_contracts) = process_files(filenames, args, detector_classes, printer_classes)
             else:
                 for filename in filenames:
+                    logger.info("current file" + filename)
                     (results_tmp, number_contracts_tmp) = process(filename, args, detector_classes, printer_classes)
                     number_contracts += number_contracts_tmp
                     results += results_tmp
