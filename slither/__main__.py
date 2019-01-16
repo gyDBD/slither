8d770abaf735cec12ae9a6d60bd78cc9e7a8e3c2
gyDBD <gydbd06@hotmail.com>
2019-01-16 13:48:03 -0500

    update exception handle

--- slither/__main__.py
+++ slither/__main__.py
@@ -266,6 +266,7 @@ def main_impl(all_detector_classes, all_printer_classes):
                 (results, number_contracts) = process_files(filenames, args, detector_classes, printer_classes)
             else:
                 for filename in filenames:
+                    logger.info("currentfile" + filename)
                     (results_tmp, number_contracts_tmp) = process(filename, args, detector_classes, printer_classes)
                     number_contracts += number_contracts_tmp
                     results += results_tmp
@@ -286,7 +287,7 @@ def main_impl(all_detector_classes, all_printer_classes):
     except Exception:
         logging.error('Error in %s' % args.filename)
         logging.error(traceback.format_exc())
-        sys.exit(-1)
+
 
 
 def parse_args(detector_classes, printer_classes):
