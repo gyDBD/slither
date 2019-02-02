db30a001c92d37f25945bfa0026b84e3c73cf5af
gyDBD <gydbd06@hotmail.com>
2019-02-02 11:08:22 -0500

    test detector

--- tests/init_success.sol
+++ tests/init_success.sol
@@ -35,7 +35,7 @@ contract C{
     i_am_a_Initialize(true);
   }
 
-  function i_am_a_Initialize(bool parameter) public onlyOwner{
+  function i_am_a_Initialize(bool parameter) public{
     if (!aa) {
       revert();
     }
